6d677097c2ee3c063fab2c8d9ae7ef0b2b3b1768
Jumar Martin <jumar@jumart.in>
2017-09-06 02:32:02 -0400

    Completed M2-L1 assignment. Added M2-L1 spec sheet.

--- Module2/M2-L1/src/com/jumarmartin/Main.java
+++ Module2/M2-L1/src/com/jumarmartin/Main.java
@@ -1,8 +1,26 @@
+/*
+ *
+ * Jumar Martin
+ * Date: 2017-09-06
+ * NCVPS (Phillip O. Berry Academy of Technology)
+ * "Hello, World!" Program
+ *
+ * Description: This is the first program for AP CompSci A SY 2017-2018.
+ * Difficulties: Forgot to apply proper formatting to "Hello, World!" (the comma between "Hello" and "World" for example)
+ * What I Learned: The foundations of a great program.
+ *
+ */
+
 package com.jumarmartin;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello, World!");
     }
 }
+
+// Sample Output
+/*
+    Hello, World!
+ */
