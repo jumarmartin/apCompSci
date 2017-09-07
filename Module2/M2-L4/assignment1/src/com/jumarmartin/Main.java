8a59dc49b756f264c4c0d4b0a52af24cef5ca1f9
Jumar Martin <jumar@jumart.in>
2017-09-07 18:56:36 -0400

    Changed spec docs to pdf, added M2-L4 assignment1

---
+++ Module2/M2-L4/assignment1/src/com/jumarmartin/Main.java
@@ -0,0 +1,32 @@
+/*
+ *
+ * NAME: Jumar Martin
+ * DATE: 2017-09-06
+ * NCVPS (Phillip O. Berry Academy of Technology)
+ * "Program 2A"
+ *
+ */
+ package com.jumarmartin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The program you");
+        System.out.println("are using is");
+        System.out.println("called Java.  It");
+        System.out.println("will soon be your friend.");
+        System.out.println("You must treat it");
+        System.out.println("very carefully.");
+    }
+}
+
+/* Sample Output
+
+The program you
+are using is
+called JAVA.  It
+will be your friend.
+You must treat it
+very carefully.
+
+ */
